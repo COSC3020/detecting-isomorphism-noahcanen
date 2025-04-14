cab58eab709658c341c55c566c375b9401ad8189
noahcanen <ncanen@uwyo.edu>
2025-04-14 15:31:16 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+function are_isomorphic(graph1, graph2) {
+    if (graph1.length != graph2.length){return false}
+    v1 = Vnum(graph1)
+    v2 = Vnum(graph2)
+    if (v1 != v2){return false}
+    if (same(graph1, graph2)){return true}
+    for (let i=0; i < graph1.length; i++){
+        if (isomorphic(graph1, graph2)){return true}
+    }
+    return false
+}
+function isomorphic(graph1, graph2){
+    a = graph1.pop()
+    graph1.unshift(a)
+    for (let i=0; i < graph1.length; i++){
+        a = graph1[i].pop()
+        graph1[i].unshift(a)
+    }
+    if (same(graph1, graph2)){return true}
+    return false
+}
+
+function same(graph1, graph2){
+    for (let i=0; i < graph1.length; i++){
+        for (let j=0; j < graph1[i].length; j++){
+            if (graph1 [i][j] != graph2[i][j]){return false}
+        }
+    }
+    return true
+}
+
+function Vnum(graph){
+    sum = 0
+    for (let i=0; i < graph.length; i++){
+        for (let j=0; j < graph[i].length; j++){
+            sum = graph[i][j] + sum
+        }
+    }
+
+    return sum
 }
