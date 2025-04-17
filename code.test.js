98d50aff32ba3f80f079dc0e56a0b373112fabb2
noahcanen <ncanen@uwyo.edu>
2025-04-16 18:25:37 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,59 +3,121 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-g1 = [
-    [0,1,1,0],
-    [1,0,0,1],
-    [1,0,0,1],
-    [0,1,1,0]
-    ];
-g2 = [
-    [0,0,1,1],
-    [0,0,1,1],
-    [1,1,0,0],
-    [1,1,0,0]
-    ];
-g3 = [
-    [1,0,1,0],
-    [0,0,1,1],
-    [1,1,0,0],
-    [1,1,0,0]
-    ];
 
-g4 = [
-    [0,1,0,0,0],
-    [1,0,1,0,0],
-    [0,1,0,1,0],
-    [0,0,1,0,1],
-    [0,0,0,1,0]
-    ];
-g5 = [
-    [0,1,0,0,1],
-    [1,0,1,0,0],
-    [0,1,0,0,0],
-    [0,0,0,0,1],
-    [1,0,0,1,0]
+const pentagon = 
+[
+    [0,1,2,3,4],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,4],
+        [4,0]
     ]
-g6 = [
-    [0,0,0,0,1],
-    [1,0,1,0,0],
-    [0,1,0,0,0],
-    [0,0,0,0,1],
-    [1,0,0,1,0]
-    ]
+];
 
-g7 = [
-    [0,0,0,1,1],
-    [1,0,1,0,0],
-    [0,1,0,0,0],
-    [0,0,0,0,1],
-    [1,0,0,1,0]
+const star = 
+[
+    [0,1,2,3,4],
+    [
+        [0,2],
+        [2,4],
+        [4,1],
+        [1,3],
+        [3,0]
     ]
-assert(are_isomorphic(g1, g2) == true)
-assert(are_isomorphic(g1, g3) == false)
-assert(are_isomorphic(g2, g3) == false)
-assert(are_isomorphic(g2, g7) == false)
-assert(are_isomorphic(g4, g5) == true)
-assert(are_isomorphic(g4, g6) == false)
-assert(are_isomorphic(g4, g7) == false)
-assert(are_isomorphic(g4, g1) == false)
+];
+
+const empty = [[],[]];
+const line = [[0,1,2],[[0,1],[1,2]]];
+
+const square = [
+    [0,1,2,3],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,0]
+    ]
+];
+
+const hourglass = [
+    [0,1,2,3],
+    [
+        [0,2],
+        [2,3],
+        [3,1],
+        [1,0]
+    ]
+];
+
+const pentaCross = [
+    [0,1,2,3,4],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,4],
+        [4,2],
+        [4,0]
+    ]
+];
+
+const complex = [
+    [0,1,2,3,4],
+    [
+        [0,1],
+        [0,3],
+        [1,2],
+        [2,0],
+        [2,3],
+        [0,4]
+    ]
+];
+
+const G = [
+    [0,1,2,3,4,5,6,7],
+    [
+        [0,1],
+        [0,3],
+        [0,5],
+        [1,2],
+        [1,4],
+        [2,3],
+        [2,7],
+        [3,6],
+        [4,5],
+        [4,7],
+        [5,6],
+        [6,7]
+    ]
+];
+
+const H = [
+    [0,1,2,3,4,5,6,7],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,0],
+        [4,5],
+        [5,6],
+        [6,7],
+        [7,4],
+        [6,2],
+        [1,5],
+        [7,3],
+        [4,0]
+    ]
+];
+
+assert(are_isomorphic(test1,test2) == (true));
+assert(are_isomorphic(test1,test3) == (false));
+assert(are_isomorphic(pentagon,star) == (true));
+assert(are_isomorphic(empty,line) == (false));
+assert(are_isomorphic(pentagon,line) == (false));
+assert(are_isomorphic(square,hourglass) == (true));
+assert(are_isomorphic(square == pentagon) == (false));
+assert(are_isomorphic(pentaCross == complex) == (false));
+assert(are_isomorphic(G,H) == (true));
+assert(are_isomorphic(G,G) == (true));
